b278c99d5569ebc474155e4a2d3647881d161e6e
ngf93 <nabiullinaguzel93@gmail.com>
2021-06-09 18:02:41 +0300

    in process 2

--- js/main.js
+++ js/main.js
@@ -4,20 +4,21 @@ let cur_year = now.getFullYear();
 document.querySelector('#year').innerHTML = cur_year;
 
 
-let anchors = Array.from(document.querySelectorAll('.to-anchor'));
-anchors.forEach(function(item, i, arr) {
-  item.addEventListener('click', () => {
-    document.querySelectorAll(item.href).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  });
-});
+// let anchors = Array.from(document.querySelectorAll('.to-anchor'));
+// anchors.forEach(function(item, i, arr) {
+//   item.addEventListener('click', () => {
+//     document.querySelectorAll(item.href).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     });
+//   });
+// });
 
 
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-reviews', {
   loop: true,
   slidesPerView: 2,
+  spaceBetween: 50,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -25,3 +26,7 @@ const swiper = new Swiper('.swiper-container', {
 });
 
 const myViewer = new ImgPreviewer('.img-preview-box');
+
+function fav(el){
+  el.dataset.fav = (el.dataset.fav == 'false') ? 'true' : 'false'
+}
